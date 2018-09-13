0f909d87b72408aa54fd18456f90f914240c46bf
Irving Didier <bider88@gmail.com>
2018-09-13 16:36:12 -0500

    Agregando el apoyo y slides

--- src/pages/help/help.ts
+++ src/pages/help/help.ts
@@ -1,12 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the HelpPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides, AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -15,11 +8,89 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HelpPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild (Slides) slides: Slides;
+
+  option: number = 0;
+  alias: string = '';
+  aliasTemp = '';
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private alertCtrl: AlertController
+    ) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HelpPage');
+    // Bloquemos el slide al usuario
+    this.slides.lockSwipes(true);
+    this.slides.freeMode = false;
   }
 
+  validOption() {
+    const indexPage = this.slides.getActiveIndex();
+
+    console.log(indexPage);
+
+
+
+    switch ( indexPage ) {
+      case 0:
+        if ( this.option == 0 ) {
+
+          this.next();
+
+        } else {
+          if ( this.option == 1 ) {
+            if ( this.setAlias() ) {
+              this.next();
+            } else {
+              this.showAlert('Ingrese alias', 'No se ha ingresado ningún alias').present();
+            }
+          }
+        }
+      break;
+      case 1:
+      break;
+      case 2:
+      break;
+    }
+  }
+
+  setAlias() {
+    const aliasTemp = this.aliasTemp.trim();
+    if ( aliasTemp.length > 0 ) {
+      this.alias = aliasTemp;
+      return true;
+    }
+
+    return false;
+  }
+
+  next() {
+    this.slides.lockSwipes(false);
+    this.slides.freeMode = true;
+
+    this.slides.slideNext();
+
+    this.slides.lockSwipes(true);
+    this.slides.freeMode = false;
+  }
+
+  back() {
+    this.slides.lockSwipes(false);
+    this.slides.freeMode = true;
+
+    this.slides.slidePrev();
+
+    this.slides.lockSwipes(true);
+    this.slides.freeMode = false;
+  }
+
+  showAlert(subTitle: string = 'Aviso', message: string = '') {
+    return this.alertCtrl.create({
+      subTitle,
+      message
+    });
+  };
 }
--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the InfoPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-info',
@@ -16,10 +9,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class InfoPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad InfoPage');
   }
 
 }
