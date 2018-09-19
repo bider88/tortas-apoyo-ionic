2e8ebf534b3e7dbaef0cddd8bc4dcac627b2a5c3
Irving Didier <bider88@gmail.com>
2018-09-19 10:23:17 -0500

    Implementando Firebase

--- src/config/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCdOoD91yerbf2qFmWppHGlO2hPlwV-BEQ',
-  authDomain: 'tortas-help.firebaseapp.com',
-  databaseURL: 'https://tortas-help.firebaseio.com',
-  projectId: 'tortas-help',
-  storageBucket: 'tortas-help.appspot.com',
-  messagingSenderId: '445569983985'
-};
